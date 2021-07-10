05ae4f9903bfd3f83dc3abcf145bfd3fd5be7a5e
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2021-07-11 00:58:31 +0700

    filter by genre done kurang search by query mapping dan benerin data crawling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from services import Home
 from services.Home import Home
 
@@ -7,7 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    discover = Home.get_random_data_discover()
+    print('Request = ',str(request.args.get('genre')))
+
+    discover = Home.get_random_data_discover(request.args.get('genre'))
     genre = Home.get_random_data_genre()
     return render_template('home.html',genre=genre,discover=discover)
 
--- services/Home.py
+++ services/Home.py
@@ -1,24 +1,37 @@
 import random
-
 import pandas as pd
+
 
 class Home():
     @classmethod
+    def get_random_data_discover(self, request):
+        dataframe = pd.read_csv('data_film.csv')
+        # cek genre request
+        if request is None:
+            print('Masuk None')
+            movies = dataframe
+        elif request == "":
+            print('Masuk String')
+            movies = dataframe
+        else:
+            print('Masuk Found')
+            movies = dataframe[dataframe['genre'] == str(request)]
 
-    def get_random_data_discover(*self):
-        movies = pd.read_csv('data_film.csv')
-        data= []
-        for i in range(0,len(movies)):
-            title = movies['title'][i]
-            description = movies['description'][i]
-            genre =  movies['genre'][i]
-            url =  movies['url'][i]
-            thumbnail =  movies['thumbnail'][i]
-            movie = {'title':title,'description':description,'genre':genre,'url':url,'thumbnail':thumbnail}
-            print('Movie Item =',movie)
+        # variabel data temporary
+        data = []
+        # loop data movies from dataset by filter genre or without genre
+        for index,row in movies.iterrows():
+            title = row['title']
+            description = row['description']
+            genre = row['genre']
+            year = row['year']
+            url = row['url']
+            thumbnail = row['thumbnail']
+            movie = {'title': title, 'description': description, 'genre': genre, 'url': url, 'thumbnail': thumbnail,
+                     'year': year}
             data.append(movie)
-
-        return []
+        # return data movies random
+        return random.sample(data, 20)
 
     def get_random_data_genre(*self):
         genre = pd.read_csv('genre.csv')
