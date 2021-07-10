d518f34a4bf060bc912a58196dc9997aadad9ec9
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2021-07-11 02:32:36 +0700

    searching data done kurang benahi datasets

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from services import Home
 from services.Home import Home
 
@@ -7,11 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print('Request = ',str(request.args.get('genre')))
+    print('Request = ', str(request.args.get('genre')))
 
     discover = Home.get_random_data_discover(request.args.get('genre'))
     genre = Home.get_random_data_genre()
-    return render_template('home.html',genre=genre,discover=discover)
+    return render_template('home.html', genre=genre, discover=discover)
+
+
+@app.route('/search')
+def search():
+    data = Home.search_data(request.args.get('q'))
+    genre = Home.get_random_data_genre()
+    return render_template('home.html', genre=genre, discover=data)
 
 
 if __name__ == '__main__':
--- services/Home.py
+++ services/Home.py
@@ -1,5 +1,11 @@
 import random
 import pandas as pd
+from operator import itemgetter
+from math import log
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import pandas as pd
 
 
 class Home():
@@ -20,7 +26,7 @@ class Home():
         # variabel data temporary
         data = []
         # loop data movies from dataset by filter genre or without genre
-        for index,row in movies.iterrows():
+        for index, row in movies.iterrows():
             title = row['title']
             description = row['description']
             genre = row['genre']
@@ -39,3 +45,87 @@ class Home():
         for i in genre['genre']:
             data.append(i)
         return data
+
+    def search_data(request):
+        word = request
+        stop_words = stopwords.words('english')
+        stemmer = PorterStemmer()
+        i = 1
+        data = pd.read_csv('data_film.csv', sep=',')  # data real sebelum di prosesing
+        # data_after = pd.read_csv('post_processing_data_film_cleaned.csv',sep="|")
+        data_after = pd.read_csv('post_processing_data_film_stemmed.csv', sep="|")
+
+        data_word = word.split(" ")
+
+        # query
+        query_word = []
+        for dw in data_word:
+            token = word_tokenize(dw)
+            dw_cleaned_token = ""
+            for word_token in token:
+                if word_token not in stop_words:
+                    dw_cleaned_token += word_token + " "
+
+            token_stemming = word_tokenize(dw_cleaned_token)
+            for ws in token_stemming:
+                query_word.append(stemmer.stem(ws))
+
+        # TF - DF
+        termFreq = []
+        docsFreq = []
+        for i, term in enumerate(query_word):  # untuk looping kata-kata inputan dari user
+            docsCounter = []
+            docsFreqCounter = 0
+            for doc in data_after['word']:  # looping untuk document per baris
+                # doc 1
+                # doc 2
+                # doc 3
+                counter = 0
+                for word in doc.split(' '):  # looping kata-kata dari dokumen perbaris
+                    if word == term:  # ini untuk cek apakah kata-kata sama atau relevan
+                        counter += 1
+                docsCounter.append(counter)
+
+                if counter > 0:
+                    docsFreqCounter += 1
+            termFreq.append(docsCounter)
+            docsFreq.append(docsFreqCounter)
+
+        # IDF + 1
+        idf_one = []
+        docsTotal = len(data)
+        for i in docsFreq:
+            idf_one.append(log(docsTotal) / (i + 1))
+
+        # TFIDF
+        docs_weight = termFreq[:]  # ['word','cutting']
+        for i in range(len(docs_weight)):
+            for j in range(len(docs_weight[i])):
+                docs_weight[i][j] = termFreq[i][j] * idf_one[i]
+
+        total_weight = []
+        for i in range(len(docs_weight[0])):
+            total_weight.append([i, 0])
+
+        for i in range(len(docs_weight)):
+            for j in range(len(docs_weight[i])):
+                total_weight[j][1] += docs_weight[i][j]
+
+        # sort document relevan
+        total_weight = sorted(total_weight, key=itemgetter(1), reverse=True)
+
+        # show top 32 document relevan
+        data_movie = []
+        for i in range(0, 32):
+            title = data['title'][total_weight[i][0]]
+            description = data['description'][total_weight[i][0]]
+            genre = data['genre'][total_weight[i][0]]
+            year = data['year'][total_weight[i][0]]
+            url = data['url'][total_weight[i][0]]
+            thumbnail = data['thumbnail'][total_weight[i][0]]
+            movie = {'title': title, 'description': description, 'genre': genre, 'url': url, 'thumbnail': thumbnail,
+                     'year': year}
+            print('Item Movies = ',movie)
+            data_movie.append(movie)
+
+        return data_movie
